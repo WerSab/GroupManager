ac58bc0f95eb8713c165ec9e05ef3f04f32830ee
Weronika Sabor <weronika.sabor@interia.pl>
2021-08-25 14:19:39 +0200

    firebase installation

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,33 +1,21 @@
-import React  from 'react';
-import {
-    StyleSheet,
-    TextInput,
-    View,
-    Text,
-    ScrollView,
-    Keyboard,
-    TouchableOpacity,
-    KeyboardAvoidingView,
-    Button,
-  } from 'react-native';
-  
-    const Main = ({navigation}) => {
-    
-      return (
-        <View style={styles.mainBody}>
-          <Text>Hello</Text>
-        </View>
-      );
-    }
-      
-  export default Main;
-  
-  const styles = StyleSheet.create({
-    mainBody: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#1a112b',
-      alignContent: 'center',
-    }}
-    );
-  
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const Main = () => {
+  return (
+    <View style={styles.mainBody}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#1a112b',
+    alignContent: 'center',
+  },
+});
