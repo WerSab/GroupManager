c8a4b5e32ebeea6020ef390e4a3b3391eca8288c
WerSab <weronika.sabor@interia.pl>
2021-10-31 22:23:00 +0100

    Tournaments_collection

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 export const FIRESTORE_COLLECTION = {
     USERS: 'users',
+    TOURNAMENTS: 'tournaments',
 };
 
 export const FIRESTORE_ROLES = {
     PLAYER: 'player',
     MANAGER: 'manager',
 }
+
