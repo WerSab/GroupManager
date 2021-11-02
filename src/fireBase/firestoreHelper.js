d63c568584a4c99f6be84bcd4e27c047b61bf3b2
WerSab <weronika.sabor@interia.pl>
2021-11-02 21:17:08 +0100

    map - querysnapshot

--- src/fireBase/firestoreHelper.js
+++ src/fireBase/firestoreHelper.js
@@ -1,9 +1,10 @@
 import firestore from '@react-native-firebase/firestore';
 
 export function getCollection (collectionName) {
+  console.log(collectionName)
+  //opakowac w promisa i zrobić cacha
   return  firestore()
     .collection(collectionName)
-    .get()
 };
 //pobieramy referencję do naszej kolekcji
 
